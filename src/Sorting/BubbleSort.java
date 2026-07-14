f5e60baeb75c743d402df1619262d67d84a5a329
Ameera <ameeraamee790@gmail.com>
2026-07-14 12:03:39 +0530

    Basics of java

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,17 @@
+package Sorting;
+public class BubbleSort {
+    static void main() {
+        int[] arr = {9,5,6,8,3,2,10};
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            System.out.println(arr[i]);
+        }
+    }
+}
